b6a6261597540f1c99d809e6dfae7f06fef706b6
Shadi Boomi <shadi.boomi@gmail.com>
2021-12-10 17:31:51 +0100

    aoc 2021 day 8 python done

--- code/2021/python/src/day8.py
+++ code/2021/python/src/day8.py
@@ -1,26 +1,151 @@
 from src import DATA_DIR
 from typing import List, Tuple
-from collections import defaultdict, Counter
+from collections import Counter
 
 
-seven_segments = [
-    " aaaa \nb    c\nb    c\n .... \ne    f\ne    f\n gggg ",
-    " .... \n.    c\n.    c\n .... \n.    f\n.    f\n .... ",
-    " aaaa \n.    c\n.    c\n dddd \ne    .\ne    .\n gggg ",
-    " aaaa \n.    c\n.    c\n dddd \n.    f\n.    f\n gggg ",
-    " .... \nb    c\nb    c\n dddd \n.    f\n.    f\n .... ",
-    " aaaa \nb    .\nb    .\n dddd \n.    f\n.    f\n gggg ",
-    " aaaa \nb    .\nb    .\n dddd \ne    f\ne    f\n gggg ",
-    " aaaa \n.    c\n.    c\n .... \n.    f\n.    f\n .... ",
-    " aaaa \nb    c\nb    c\n dddd \ne    f\ne    f\n gggg ",
-    " aaaa \nb    c\nb    c\n dddd \n.    f\n.    f\n gggg ",
-]
+class SegmentDigit:
+    def __init__(self, *sargs):
+        if len(sargs) != 7:
+            raise ValueError("SegmentDigit must be initialized with 9 arguments")
 
-pattern_list = ["".join(sorted([s for s in list(set(seg)) if s not in ["\n", ".", " "]])) for seg in seven_segments]
+        self.a, self.b, self.c, self.d, self.e, self.f, self.g = sargs
+        self.digit_rep = [
+            f" {self.a}{self.a}{self.a}{self.a} \n{self.b}    {self.c}\n{self.b}    {self.c}\n .... \n{self.e}    {self.f}\n{self.e}    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+            f" .... \n.    {self.c}\n.    {self.c}\n .... \n.    {self.f}\n.    {self.f}\n .... ",
+            f" {self.a}{self.a}{self.a}{self.a} \n.    {self.c}\n.    {self.c}\n {self.d}{self.d}{self.d}{self.d} \n{self.e}    .\n{self.e}    .\n {self.g}{self.g}{self.g}{self.g} ",
+            f" {self.a}{self.a}{self.a}{self.a} \n.    {self.c}\n.    {self.c}\n {self.d}{self.d}{self.d}{self.d} \n.    {self.f}\n.    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+            f" .... \n{self.b}    {self.c}\n{self.b}    {self.c}\n {self.d}{self.d}{self.d}{self.d} \n.    {self.f}\n.    {self.f}\n .... ",
+            f" {self.a}{self.a}{self.a}{self.a} \n{self.b}    .\n{self.b}    .\n {self.d}{self.d}{self.d}{self.d} \n.    {self.f}\n.    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+            f" {self.a}{self.a}{self.a}{self.a} \n{self.b}    .\n{self.b}    .\n {self.d}{self.d}{self.d}{self.d} \n{self.e}    {self.f}\n{self.e}    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+            f" {self.a}{self.a}{self.a}{self.a} \n.    {self.c}\n.    {self.c}\n .... \n.    {self.f}\n.    {self.f}\n .... ",
+            f" {self.a}{self.a}{self.a}{self.a} \n{self.b}    {self.c}\n{self.b}    {self.c}\n {self.d}{self.d}{self.d}{self.d} \n{self.e}    {self.f}\n{self.e}    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+            f" {self.a}{self.a}{self.a}{self.a} \n{self.b}    {self.c}\n{self.b}    {self.c}\n {self.d}{self.d}{self.d}{self.d} \n.    {self.f}\n.    {self.f}\n {self.g}{self.g}{self.g}{self.g} ",
+        ]
 
-n_digits = defaultdict(list)
-for idx, pattern in enumerate(pattern_list):
-    n_digits[len(pattern)].append(idx)
+    def __getitem__(self, idx) -> str:
+        if not (0 <= idx <= 9):
+            raise IndexError("Index must be in range [0,9]")
+
+        return self.digit_rep[idx]
+
+
+class SegmentDecoder:
+    N_SEGMENTS = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
+    LEN_SEGMENT_DISTRIBUTION = {6: [0, 6, 9], 2: [1], 5: [2, 3, 5], 4: [4], 3: [7], 7: [8]}
+
+    def __init__(self, signal_patterns):
+        if len(signal_patterns) != 10:
+            raise ValueError("SegmentDecoder's `signal_patterns` must have 10 arguments")
+        self.signal_patterns = signal_patterns
+        self.len_signal = [len(signal_pattern) for signal_pattern in signal_patterns]
+        self.idx_patterns = ["" for _ in range(10)]
+
+    def find_unique_digits(self):
+        """Finds which patterns have a unique number of segments and associate them to
+        positions 1, 4, 7 and 8.
+        """
+        for n_segs, num in self.LEN_SEGMENT_DISTRIBUTION.items():
+            if len(num) == 1:
+                idx_unique_digit = self.len_signal.index(n_segs)
+                self.idx_patterns[num[0]] = self.signal_patterns[idx_unique_digit]
+
+    def find_upper_segment(self):
+        """Assign the first segment letter by comparing pattern 1 and 7"""
+        pat_one = self.idx_patterns[1]
+        pat_seven = self.idx_patterns[7]
+        self.a = list(set(pat_seven).difference(set(pat_one)))[0]
+
+    def find_right_segments(self):
+        """Detects right segments from 1 by comparing digits 8 and 6."""
+        pat_one = self.idx_patterns[1]
+        pat_eight = self.idx_patterns[8]
+        for one_letter in pat_one:
+            pot_pat_six = "".join([letter for letter in pat_eight if letter != one_letter])
+            for pattern in self.signal_patterns:
+                if sorted(pattern) == sorted(pot_pat_six):
+                    self.idx_patterns[6] = pattern
+                    self.c = one_letter
+                    self.f = [letter for letter in pat_one if letter != one_letter][0]
+                    return
+
+    def find_middle_top_left_segments(self):
+        """Detects middle and top left segments from the difference between
+        1 and 4 by comparing 0 and 8
+        """
+        pat_one = self.idx_patterns[1]
+        pat_eight = self.idx_patterns[8]
+        pat_four = self.idx_patterns[4]
+
+        top_left_segments = "".join(list(set(pat_four).difference(set(pat_one))))
+        for four_letter in top_left_segments:
+            pot_pat_zero = "".join([letter for letter in pat_eight if letter != four_letter])
+            for pattern in self.signal_patterns:
+                if sorted(pattern) == sorted(pot_pat_zero):
+                    self.idx_patterns[0] = pattern
+                    self.d = four_letter
+                    self.b = [letter for letter in top_left_segments if letter != four_letter][0]
+                    return
+
+    def find_bottom_left_segment(self):
+        six_seg_pats = [self.idx_patterns[0], self.idx_patterns[6]]
+        pat_nine = ""
+        pat_eight = self.idx_patterns[8]
+        for len_pattern, pattern in zip(self.len_signal, self.signal_patterns):
+            if len_pattern == 6 and pattern not in six_seg_pats:
+                pat_nine = pattern
+                break
+        self.idx_patterns[9] = pat_nine
+        self.e = list(set(pat_eight).difference(set(pat_nine)))[0]
+        pat_four = self.idx_patterns[4]
+        top_bottom_segments = "".join(list(set(pat_nine).difference(set(pat_four))))
+        self.g = [letter for letter in top_bottom_segments if letter != self.a][0]
+
+    def reassociate_five_seg_patterns(self):
+        for len_pattern, pattern in zip(self.len_signal, self.signal_patterns):
+            if len_pattern == 5:
+                if sorted("".join([self.a, self.c, self.d, self.e, self.g])) == sorted(pattern):
+                    self.idx_patterns[2] = pattern
+                elif sorted("".join([self.a, self.c, self.d, self.f, self.g])) == sorted(pattern):
+                    self.idx_patterns[3] = pattern
+                elif sorted("".join([self.a, self.b, self.d, self.f, self.g])) == sorted(pattern):
+                    self.idx_patterns[5] = pattern
+
+    def guess_segments(self):
+        """Combine guessing signals in that order"""
+        self.find_unique_digits()
+        self.find_upper_segment()
+        self.find_right_segments()
+        self.find_middle_top_left_segments()
+        self.find_bottom_left_segment()
+        self.reassociate_five_seg_patterns()
+
+    def create_segment_digit(self):
+        self.segdig = SegmentDigit(self.a, self.b, self.c, self.d, self.e, self.f, self.g)
+
+    def show_pattern(self):
+        if not hasattr(self, "segdig"):
+            self.create_segment_digit()
+        return self.segdig[8]
+
+    def decode_signal(self, signal: List[str]) -> int:
+        """Decodes the signal using the complete list of patterns
+
+        Parameters
+        ----------
+        signal : List[str]
+            [description]
+
+        Returns
+        -------
+        int
+            [description]
+        """
+        arranged_patterns = ["".join(sorted(pattern)) for pattern in self.idx_patterns]
+        output_value = ""
+        for digit in signal:
+            output_value += str(arranged_patterns.index("".join(sorted(digit))))
+
+        return int(output_value)
 
 
 def load_data() -> Tuple[List[List[str]], List[List[str]]]:
@@ -40,25 +165,27 @@ def find_unique_n_digits(digit_output: List[str]) -> int:
     for digit_sample in digit_output:
         len_counter += Counter([len(samp) for samp in digit_sample])
 
-    unique_lens = [n_segs for n_segs, num in n_digits.items() if len(num) == 1]
+    unique_lens = [n_segs for n_segs, num in SegmentDecoder.LEN_SEGMENT_DISTRIBUTION.items() if len(num) == 1]
     return sum([len_count for digit_len, len_count in len_counter.items() if digit_len in unique_lens])
 
 
-def decode_signal(digit_output: List[str]) -> int:
-    signal_output = ""
-    for digit in digit_output:
-        current_digit = pattern_list.index("".join(sorted(digit)))
-        signal_output += str(current_digit)
-    return int(signal_output)
+def sum_output_values(signal_patterns: List[List[str]], digit_output: List[List[str]]) -> int:
+    total_sum = 0
+    for signal_pattern, digits in zip(signal_patterns, digit_output):
+        sd = SegmentDecoder(signal_pattern)
+        sd.guess_segments()
+
+        total_sum += sd.decode_signal(digits)
+    return total_sum
 
 
 def day8():
     print("--- Day 8: Seven Segment Search ---")
     signal_patterns, digit_output_value = load_data()
-    print(pattern_list)
-    print(n_digits)
     unique_n_digits = find_unique_n_digits(digit_output_value)
     print("Number of times we can find 1, 4, 7 or 8:", unique_n_digits)
+    total_output = sum_output_values(signal_patterns, digit_output_value)
+    print("Total output value", total_output)
 
 
 if __name__ == "__main__":
--- code/2021/python/test/test_day8.py
+++ code/2021/python/test/test_day8.py
@@ -1,5 +1,5 @@
 import pytest
-from src.day8 import find_unique_n_digits, decode_signal
+from src.day8 import find_unique_n_digits, SegmentDecoder, sum_output_values
 
 
 def test_find_unique_n_digits():
@@ -22,10 +22,105 @@ def test_find_unique_n_digits():
     assert act_n_instances == exp_n_instances, f"Expected {exp_n_instances}. Got {act_n_instances}"
 
 
+def test_show_pattern_formation():
+    test_pattern = ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"]
+
+    sd = SegmentDecoder(test_pattern)
+    sd.guess_segments()
+    act_pattern = sd.show_pattern()
+
+    exp_pattern = " dddd \ne    a\ne    a\n ffff \ng    b\ng    b\n cccc "
+    assert act_pattern == exp_pattern, f"Expected:\n{exp_pattern}\nGot:\n{act_pattern}"
+
+
+def test_sort_patterns():
+    test_pattern = ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"]
+
+    sd = SegmentDecoder(test_pattern)
+    sd.guess_segments()
+    act_idx_pattern = sd.idx_patterns
+    exp_idx_pattern = ["cagedb", "ab", "gcdfa", "fbcad", "eafb", "cdfbe", "cdfgeb", "dab", "acedgfb", "cefabd"]
+
+    assert act_idx_pattern == exp_idx_pattern, f"Expected:\n{exp_idx_pattern}\nGot:\n{act_idx_pattern}"
+
+
 def test_decode_signal_simple():
+    test_pattern = ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"]
     test_signal = ["cdfeb", "fcadb", "cdfeb", "cdbaf"]
 
-    act_signal = decode_signal(test_signal)
+    sd = SegmentDecoder(test_pattern)
+    sd.guess_segments()
+
+    act_signal = sd.decode_signal(test_signal)
     exp_signal = 5353
 
     assert act_signal == exp_signal, f"Expected {exp_signal}. Got {act_signal}"
+
+
+def test_decode_signal_multiple_instances():
+    signal_instances = [
+        ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"],
+        ["edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"],
+        ["fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"],
+        ["fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"],
+        ["aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga"],
+        ["fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf"],
+        ["dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"],
+        ["bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd"],
+        ["egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"],
+        ["gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"],
+    ]
+    digit_instances = [
+        ["fdgacbe", "cefdb", "cefbgd", "gcbe"],
+        ["fcgedb", "cgb", "dgebacf", "gc"],
+        ["cg", "cg", "fdcagb", "cbg"],
+        ["efabcd", "cedba", "gadfec", "cb"],
+        ["gecf", "egdcabf", "bgf", "bfgea"],
+        ["gebdcfa", "ecba", "ca", "fadegcb"],
+        ["cefg", "dcbef", "fcge", "gbcadfe"],
+        ["ed", "bcgafe", "cdgba", "cbgef"],
+        ["gbdfcae", "bgc", "cg", "cgb"],
+        ["fgae", "cfgab", "fg", "bagce"],
+    ]
+
+    exp_signal_list = [8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
+
+    for idx, (test_pattern, test_signal) in enumerate(zip(signal_instances, digit_instances)):
+        sd = SegmentDecoder(test_pattern)
+        sd.guess_segments()
+
+        act_signal = sd.decode_signal(test_signal)
+        exp_signal = exp_signal_list[idx]
+
+        assert act_signal == exp_signal, f"Instance {idx}. Expected {exp_signal}. Got {act_signal}"
+
+
+def test_sum_output_values():
+    signal_instances = [
+        ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"],
+        ["edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"],
+        ["fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"],
+        ["fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"],
+        ["aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga"],
+        ["fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf"],
+        ["dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"],
+        ["bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd"],
+        ["egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"],
+        ["gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"],
+    ]
+    digit_instances = [
+        ["fdgacbe", "cefdb", "cefbgd", "gcbe"],
+        ["fcgedb", "cgb", "dgebacf", "gc"],
+        ["cg", "cg", "fdcagb", "cbg"],
+        ["efabcd", "cedba", "gadfec", "cb"],
+        ["gecf", "egdcabf", "bgf", "bfgea"],
+        ["gebdcfa", "ecba", "ca", "fadegcb"],
+        ["cefg", "dcbef", "fcge", "gbcadfe"],
+        ["ed", "bcgafe", "cdgba", "cbgef"],
+        ["gbdfcae", "bgc", "cg", "cgb"],
+        ["fgae", "cfgab", "fg", "bagce"],
+    ]
+
+    act_sum = sum_output_values(signal_instances, digit_instances)
+    exp_sum = 61229
+    assert act_sum == exp_sum, f"Expected {exp_sum}. Got {act_sum}"
