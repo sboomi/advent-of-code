66ae4d755889e3782a5a94299c188f6af10f5a30
Shadi Boomi <shadi.boomi@gmail.com>
2022-10-23 15:54:46 +0200

    added JS and Java baselines

--- code/2021/python/src/day15.py
+++ code/2021/python/src/day15.py
@@ -1,16 +1,46 @@
+from typing import List, Tuple
+from collections import namedtuple
+
 from src import DATA_DIR
 
+START_POINT = (0, 0)
+END_POINT = lambda cavern_map: (len(cavern_map[0]) - 1, len(cavern_map) - 1)
 
-def load_data():
+NodeValue = namedtuple("NodeValue", ["point", "value"])
+
+
+def load_data() -> List[List[int]]:
+    """Loads the map of the cavern, where
+    each value represents the risk level.
+
+    Returns
+    -------
+    List[List[int]]
+        2D map of the cavern
+    """
     day_fifteen_file = DATA_DIR / "day15"
     with open(day_fifteen_file, "r") as f:
-        return f.read()
+        return [[int(el) for el in line] for line in f.read().split("\n")]
+
+
+def next_nodes(point: Tuple[int, int], cavern_map: List[List[int]]) -> List[NodeValue]:
+    x, y = point
+    x_max, y_max = END_POINT(cavern_map)
+
+    right_node = NodeValue((x + 1, y), cavern_map[y][x + 1]) if x <= x_max else None
+    down_node = NodeValue((x, y + 1), cavern_map[y + 1][x]) if y <= y_max else None
+    return [node for node in (right_node, down_node) if node]
+
+
+def calculate_lowest_total_risk(cavern_map: List[List[int]]) -> int:
+    return 0
 
 
 def day15():
     print("--- Day 15: Chiton ---")
-    data = load_data()
-    print(data)
+    cavern_map = load_data()
+    print("\n".join(["".join([str(risk_lv) for risk_lv in cavern_row]) for cavern_row in cavern_map]))
+    print(f"Map dims: {len(cavern_map[0])} x {len(cavern_map)}")
 
 
 if __name__ == "__main__":
---
+++ code/2021/python/test/test_day15.py
@@ -0,0 +1,22 @@
+import pytest
+from src.day15 import calculate_lowest_total_risk
+
+
+def test_calculate_lowest_total_risk():
+    cavern_map = [
+        [1, 1, 6, 3, 7, 5, 1, 7, 4, 2],
+        [1, 3, 8, 1, 3, 7, 3, 6, 7, 2],
+        [2, 1, 3, 6, 5, 1, 1, 3, 2, 8],
+        [3, 6, 9, 4, 9, 3, 1, 5, 6, 9],
+        [7, 4, 6, 3, 4, 1, 7, 1, 1, 1],
+        [1, 3, 1, 9, 1, 2, 8, 1, 3, 7],
+        [1, 3, 5, 9, 9, 1, 2, 4, 2, 1],
+        [3, 1, 2, 5, 4, 2, 1, 6, 3, 9],
+        [1, 2, 9, 3, 1, 3, 8, 5, 2, 1],
+        [2, 3, 1, 1, 9, 4, 4, 5, 8, 1],
+    ]
+
+    exp_lowest_risk = 40
+    act_lowest_risk = calculate_lowest_total_risk(cavern_map)
+
+    assert exp_lowest_risk == act_lowest_risk, f"Expected {exp_lowest_risk}. Got {act_lowest_risk}"
